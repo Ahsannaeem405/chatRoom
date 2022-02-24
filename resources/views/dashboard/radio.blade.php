ff0436b18acf3f761f81e61dc2aa908d0862eb01
Talha Bhatti <80376242+talhabhathi@users.noreply.github.com>
2022-02-24 18:36:57 +0500

    b

--- resources/views/dashboard/radio.blade.php
+++ resources/views/dashboard/radio.blade.php
@@ -147,7 +147,7 @@ active
                 </div>
                 <div class="modal-footer">
                   {{-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> --}}
-                  <button type="submit" class="btn btn-primary">Add Images</button>
+                  <button type="submit" class="btn btn-primary">Update Radio Setting</button>
 
                 </div>
               </div>
