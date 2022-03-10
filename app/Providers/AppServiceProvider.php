89966d198a9cdb6780cff162b2be9c759a19e887
rehman101 <73483363+rehman101@users.noreply.github.com>
2022-03-10 14:53:02 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
        URL::forceScheme('https');
+       
         $social = socialSetting::first();
         $google = [
             'client_id' => $social->googleClient,
