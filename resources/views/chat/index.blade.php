a72d0501868e49ccc04fc9e8c6420cdb296458b8
Abu Sufian <60252207+RaiSufian@users.noreply.github.com>
2022-03-21 12:21:22 +0500

    set dropdown on mobile

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -772,6 +772,13 @@ die();
                                                 </div>
                                             @endforeach
                                             
+                                            @foreach($giftenor->results as $gift)
+                                                    <div class="col-3 mt-2">
+                                                        <img src="{{$gift->media[0]->gif->url}}" style="width: 100%;height: 150px"
+                                                            gifid="{{$gift->media[0]->gif->url}}" type="tenor" class="img-fluid gifupload"/>
+                                                    </div>
+                                             @endforeach 
+                                            
                                              
                                             
                                             
