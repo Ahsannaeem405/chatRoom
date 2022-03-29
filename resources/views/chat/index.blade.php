e125c05941181ce6e2b93c461e32e8941cbb50ba
Abu Sufian <60252207+RaiSufian@users.noreply.github.com>
2022-03-29 17:36:26 +0500

    emoji div location

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -40,7 +40,11 @@
                 right:94px;
                 top:27px;
             }
-            
+            .emoji-menu{
+                right: 0px;
+                width: 300px;
+                top: 220px;
+                }
     </style>
 
 
